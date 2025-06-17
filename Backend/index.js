0e94dcf9502403fb9b21c7ad05493447978077d5
Dharmateja69 <ddharmateja69@gmail.com>
2025-06-18 01:00:02 +0530

    Changed the backend url also

--- Backend/index.js
+++ Backend/index.js
@@ -88,5 +88,5 @@ app.use(errorHandler);
 // Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:5000`);
+    console.log(`Server running on https://studysync-backend-7iry.onrender.com/`);
 });
--- Frontend/src/lib/api.ts
+++ Frontend/src/lib/api.ts
@@ -2,14 +2,14 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://studysync-backend-7iry.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
   withCredentials: true,
 });
 export const apiformdata = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://studysync-backend-7iry.onrender.com/api",
   withCredentials: true,
   // 🚫 Don't hardcode "Content-Type" here — Axios handles it per request type
 });
